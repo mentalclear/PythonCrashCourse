be951e70ffb1ac028e036d921bf1ce0bacf1a803
Dmitrii Kilishek <mentalclear@gmail.com>
2021-08-08 20:50:02 -0400

    Partially done with Chapter 8 completed exercise 8_8, continue with Chapter 8 - Passing a list

---
+++ Project/Chapter8/Exercises/exercise8_3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, message):
+    print("Your shirt will be size " + size + " with a message " + message)
+
+
+make_shirt("XL", "Just Do It!")
+make_shirt(size="L", message="Run Happy")
