ec2bc1695948c82705d43a37442e9229525db518
Dmitrii Kilishek <mentalclear@gmail.com>
2021-08-09 20:07:06 -0400

    Continue Chapter 8 with "Passing an arbitrary number of arguments"

---
+++ Project/Chapter8/Exercises/exercise8_9.py
@@ -0,0 +1,7 @@
+def show_magicians(list_of_magicians):
+    for magician in list_of_magicians:
+        print("Magician's name: " + magician.title())
+
+
+magicians = ['david copperfield', 'doug henning', 'lance burton']
+show_magicians(magicians)
